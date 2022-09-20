d474a9b7bd021b4e0c87756f57326593a8733cac
SenorC4 <44325992+SenorC4@users.noreply.github.com>
2022-09-20 10:43:57 -0700

    level files and level intake

--- ContraptionZacLevel.java
+++ ContraptionZacLevel.java
@@ -3,7 +3,12 @@ import java.io.*;
 
 public class ContraptionZacLevel
 {
-   int[][] data = new int[9][9];
+   int[][] data;
+   int x;
+   int y;
+   int exitX;
+   int exitY;
+   
 
    String[] directions = new String[4];
    
@@ -16,18 +21,30 @@ public class ContraptionZacLevel
       try
       {
          Scanner scan = new Scanner(new File(filename));
-         for(int i=0;i<9;i++)
+         
+         //remove "Layout" string
+         scan.next()
+         
+         x = scan.nextInt()
+         y = scan.nextInt()
+         
+         //get layout
+         data = new int[x][y];
+         
+         for(int i=0;i<x;i++)
          {
-            for(int j=0;j<9;j++)
+            for(int j=0;j<y;j++)
             {
-               data[j][i] = scan.nextInt();
+               data[j][i] = scan.next();
             }
          }
-         directions[0] = scan.next();
-         directions[1] = scan.next();
-         directions[2] = scan.next();
-         directions[3] = scan.next();
-      
+         
+         //get walls
+         
+         //get objects
+         
+         
+               
       }
       catch(Exception e)
       {
